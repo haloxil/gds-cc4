d9ac450429dafe180bef325d89dbe4790d14317e
Sean Evan Seah <dance0511@gmail.com>
2023-09-09 13:53:44 +0800

    map country code to country

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ URL = (
 with urllib.request.urlopen(URL) as response:
     data = json.load(response)
 
+country_codes_df = pd.read_excel('Country-Code.xlsx')
+
 with open('restaurant.json', 'r', encoding='utf-8') as f:
     metadata = json.load(f)
 
@@ -22,14 +24,32 @@ selected_cols = [metadata['restaurant_id'], metadata['restaurant_name'],
                  metadata['user_aggregate_rating'], metadata['cuisines']]
 df = df[selected_cols]
 
+df = pd.merge(df, country_codes_df, left_on=metadata['country'],
+              right_on='Country Code', how='left')
+df = df.drop(columns={
+    metadata['country'],
+    'Country Code'
+})
+
 df = df.rename(columns={
     metadata['restaurant_id']: 'Restaurant Id',
     metadata['restaurant_name']: 'Restaurant Name',
-    metadata['country']: 'Country',
     metadata['city']: 'City',
     metadata['user_rating_votes']: 'User Rating Votes',
     metadata['user_aggregate_rating']: 'User Aggregate Rating',
     metadata['cuisines']: 'Cuisines'
 })
 
-print(df.columns)
+column_order = [
+    'Restaurant Id',
+    'Restaurant Name',
+    'City',
+    'Country',
+    'User Rating Votes',
+    'User Aggregate Rating',
+    'Cuisines'
+]
+
+df = df.reindex(columns=column_order)
+
+print(df)
