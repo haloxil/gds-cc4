c1729d882c24fb65bef00a64fafbaf19e0980a78
Sean Evan Seah <dance0511@gmail.com>
2023-09-10 14:16:14 +0800

    refactor restaurants.py

--- src/restaurants.py
+++ src/restaurants.py
@@ -6,31 +6,36 @@ import numpy as np
 import main
 
 df_restaurants, metadata = main.main()
-
 country_codes_df = pd.read_excel('Country-Code.xlsx')
 
-selected_cols = [metadata['restaurant_id'], metadata['restaurant_name'],
-                 metadata['country'], metadata['city'],
-                 metadata['user_rating_votes'],
-                 metadata['user_aggregate_rating'], metadata['cuisines']]
+restaurant_id = metadata['restaurant_id']
+restaurant_name = metadata['restaurant_name']
+country = metadata['country']
+city = metadata['city']
+user_rating_votes = metadata['user_rating_votes']
+user_aggregate_rating = metadata['user_aggregate_rating']
+cuisines = metadata['cuisines']
+
+selected_cols = [restaurant_id, restaurant_name, country, city, 
+                 user_rating_votes, user_aggregate_rating, cuisines]
 df_restaurants = df_restaurants[selected_cols]
 
-df_restaurants = pd.merge(df_restaurants, country_codes_df, left_on=metadata['country'],
+df_restaurants = pd.merge(df_restaurants, country_codes_df, left_on=country,
               right_on='Country Code', how='left')
 df_restaurants = df_restaurants.drop(columns={
-    metadata['country'],
+    country,
     'Country Code'
 })
 df_restaurants = df_restaurants.replace(r'^\s*$', np.nan, regex=True)
 df_restaurants = df_restaurants.fillna('NA')
 
 df_restaurants = df_restaurants.rename(columns={
-    metadata['restaurant_id']: 'Restaurant Id',
-    metadata['restaurant_name']: 'Restaurant Name',
-    metadata['city']: 'City',
-    metadata['user_rating_votes']: 'User Rating Votes',
-    metadata['user_aggregate_rating']: 'User Aggregate Rating',
-    metadata['cuisines']: 'Cuisines'
+    restaurant_id: 'Restaurant Id',
+    restaurant_name: 'Restaurant Name',
+    city: 'City',
+    user_rating_votes: 'User Rating Votes',
+    user_aggregate_rating: 'User Aggregate Rating',
+    cuisines: 'Cuisines'
 })
 
 column_order = [
