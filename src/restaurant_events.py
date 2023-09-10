d649ec3c851b1c2342f3ce9b9f25993bce863c6a
Sean Evan Seah <dance0511@gmail.com>
2023-09-10 15:15:51 +0800

    refactored restaurant_events.py

--- src/restaurant_events.py
+++ src/restaurant_events.py
@@ -1,9 +1,10 @@
 """
-This module extracts selected fields and store the data as restaurant_events.csv.
+This module extracts selected fields and
+store the data as restaurant_events.csv.
 """
-import pandas as pd
 from datetime import datetime, date, timedelta
-import main 
+import pandas as pd
+import main
 from utils import populate_empty_values
 
 df_restaurants, metadata = main.main()
@@ -12,30 +13,46 @@ year = metadata['rules']['date'][0]
 month = metadata['rules']['date'][1]
 day = metadata['rules']['date'][2]
 
-df_events = df_restaurants[[metadata['restaurant_id'], metadata['restaurant_name'], 
-                            metadata['photo_url'], 'restaurant.zomato_events']]
-df_events = df_events.dropna(subset=['restaurant.zomato_events'])
-df_events = df_events.explode('restaurant.zomato_events').reset_index(drop=True)
+restaurant_id = metadata['restaurant_id']
+restaurant_name = metadata['restaurant_name']
+photo_url = metadata['photo_url']
+events = metadata['events']['key']
+event_id = metadata['events']['event_id'].split('.')[1]
+event_title = metadata['events']['event_title'].split('.')[1]
+event_start_date = metadata['events']['event_start_date'].split('.')[1]
+event_end_date = metadata['events']['event_end_date'].split('.')[1]
 
-df_events['Event Id'] = df_events['restaurant.zomato_events'].apply(lambda x: x['event']['event_id'])
-df_events['Event Title'] = df_events['restaurant.zomato_events'].apply(lambda x: x['event']['title'])
-df_events['Event Start Date'] = df_events['restaurant.zomato_events'].apply(lambda x: x['event']['start_date'])
-df_events['Event End Date'] = df_events['restaurant.zomato_events'].apply(lambda x: x['event']['end_date'])
+df_events = df_restaurants[[restaurant_id, restaurant_name,
+                            photo_url, events]]
+df_events = df_events.dropna(subset=[events])
+df_events = df_events.explode(events).reset_index(drop=True)
 
-df_events['Event Start Date'] = pd.to_datetime(df_events['Event Start Date'])
-df_events['Event End Date'] = pd.to_datetime(df_events['Event End Date'])
+fields_to_extract = [event_id, event_title, event_start_date, event_end_date]
+for field in fields_to_extract:
+    df_events[field] = df_events[events].apply(lambda x: x['event'][field])
 
-df_events = df_events[(df_events['Event Start Date'] 
-        <= datetime.combine(date(year, month + 1, day), datetime.min.time()) - timedelta(days=1)) 
-        & (df_events['Event End Date'] 
-        >= datetime.combine(date(year, month, day), datetime.min.time()))]
+df_events[event_start_date] = pd.to_datetime(df_events[event_start_date])
+df_events[event_end_date] = pd.to_datetime(df_events[event_end_date])
+
+df_events = df_events[(df_events[event_start_date]
+                       <= datetime.combine(
+                           date(year, month + 1, day),
+                           datetime.min.time()) - timedelta(days=1))
+                      & (df_events[event_end_date]
+                         >= datetime.combine(
+                             date(year, month, day),
+                             datetime.min.time()))]
 
 df_events = populate_empty_values(df_events)
 
 df_events = df_events.rename(columns={
-    metadata['restaurant_id']: 'Restaurant Id',
-    metadata['restaurant_name']: 'Restaurant Name',
-    metadata['photo_url']: 'Photo URL',
+    event_id: 'Event Id',
+    restaurant_id: 'Restaurant Id',
+    restaurant_name: 'Restaurant Name',
+    photo_url: 'Photo URL',
+    event_title: 'Event Title',
+    event_start_date: 'Event Start Date',
+    event_end_date: 'Event End Date'
 })
 
 column_order = [
