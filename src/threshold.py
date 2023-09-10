7ee59a6ad97d01e985fb9818e9853ecd9bfcf031
Sean Evan Seah <dance0511@gmail.com>
2023-09-10 13:44:14 +0800

    add threshold.py

---
+++ src/threshold.py
@@ -0,0 +1,25 @@
+"""
+This module extracts selected fields and store the data as restaurant_events.csv.
+"""
+import pandas as pd
+import main 
+
+
+data, metadata = main.main()
+
+df_threshold = pd.json_normalize(data, ['restaurants'])
+selected_cols = [metadata['user_text_rating'], metadata['user_aggregate_rating']]
+df_threshold  = df_threshold [selected_cols]
+
+df_threshold[metadata['user_aggregate_rating']] = pd.to_numeric(df_threshold[metadata['user_aggregate_rating']], errors='coerce')
+min_ratings = df_threshold.groupby(metadata['user_text_rating'])[metadata['user_aggregate_rating']].min().sort_values()
+
+min_ratings_df = min_ratings.reset_index()
+min_ratings_df= min_ratings_df[min_ratings_df[metadata['user_text_rating']].isin(metadata['rules']['text_rating'])]
+
+min_ratings_df = min_ratings_df.rename(columns={
+    metadata['user_text_rating']: 'User Text Rating',
+    metadata['user_aggregate_rating']: 'User Aggregate Rating'
+})
+
+min_ratings_df.to_csv('threshold.csv', index=False, encoding='utf-8-sig')
