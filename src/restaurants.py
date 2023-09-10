52e04cf3d5e5c47bdac397ef90b30dbbb6b4703b
Sean Evan Seah <dance0511@gmail.com>
2023-09-10 14:29:22 +0800

    refactor restaurants.py

--- src/restaurants.py
+++ src/restaurants.py
@@ -10,22 +10,23 @@ country_codes_df = pd.read_excel('Country-Code.xlsx')
 
 restaurant_id = metadata['restaurant_id']
 restaurant_name = metadata['restaurant_name']
-country = metadata['country']
+country_id = metadata['country_id']
 city = metadata['city']
 user_rating_votes = metadata['user_rating_votes']
 user_aggregate_rating = metadata['user_aggregate_rating']
 cuisines = metadata['cuisines']
+country_code = 'Country Code'
 
-selected_cols = [restaurant_id, restaurant_name, country, city,
+selected_cols = [restaurant_id, restaurant_name, country_id, city,
                  user_rating_votes, user_aggregate_rating, cuisines]
 df_restaurants = df_restaurants[selected_cols]
 
-df_restaurants = pd.merge(df_restaurants, country_codes_df, left_on=country,
-                          right_on='Country Code', how='left')
+df_restaurants = pd.merge(df_restaurants, country_codes_df, left_on=country_id,
+                          right_on=country_code, how='left')
 
 df_restaurants = df_restaurants.drop(columns={
-    country,
-    'Country Code'
+    country_id,
+    country_code
 })
 df_restaurants = populate_empty_values(df_restaurants)
 
