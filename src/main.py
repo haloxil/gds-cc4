1d9103bfd38e5a837dfb23be08eec9c0e8d96d4b
Sean Evan Seah <dance0511@gmail.com>
2023-09-10 20:29:43 +0800

    add 2 methods to read files: s3 or locally

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ main.py
 import json
 import urllib.request
 import pandas as pd
+import boto3
 
 def main():
     URL = (
@@ -13,8 +14,19 @@ def main():
     with urllib.request.urlopen(URL) as response:
         data = json.load(response)
 
-    with open('restaurant.json', 'r', encoding='utf-8') as f:
+    with open('restaurant_metadata.json', 'r', encoding='utf-8') as f:
         metadata = json.load(f)
+    
+    # This method uses s3 to retrieve the files
+    '''
+    s3_client = boto3.client('s3')
+    bucket = 'govtech-cc4'
+
+    data_response = s3_client.get_object(Bucket=bucket, Key='files/restaurant.json')
+    data = json.loads(data_response['Body'].read().decode('utf-8'))
+    metadata_response = s3_client.get_object(Bucket=bucket, Key='files/restaurant_metadata.json')
+    metadata = json.loads(metadata_response['Body'].read().decode('utf-8'))
+    '''
 
     df = pd.json_normalize(data, ['restaurants'])
 
